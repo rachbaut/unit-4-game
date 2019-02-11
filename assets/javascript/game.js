2560c449132909ffd75c15f18cd2d5c9d76b7ace
Rachel Bautista <rachelbaut5@gmail.com>
2019-02-11 13:04:01 -0800

    adding to html and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
 
     //number manipulated by clicking on the crystals
-    var crystalNum = 0;
+   
 
-    //this is a randnum generated to get us to the the target number
-    var randNum = randomNumGen();
+    //this is a random number generated to get us to the the target number
+
+    var randNum = randomNumGen(); //is this a function in a variable?
 
     //setting up rest of variables
     var win = 0;
     var lose = 0;
-    var crystals; //why is this one set up like this?
+    var crystals = 0;
 
-    //this function will generate rand values for each crystal object
-    function randNumCrystals() {
-        //these are crystal objects
-        return {
-            red: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageUrl: "assets/images/ruby.jpg"
-            },
-            blue: {
-                points: Math.floor(Math.random() * 12) +1,
-                imageUrl: "assets/images/sapphire.jpg"
-            },
-            yellow: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageUrl: "assets/images/teardrop.jpg"
-            },
-            green: {
-                points: Math.floor(Math.random() * 12) + 1,
-                imageUrl: "assets/images/tourmaline.jpg"
-            }
-        };
-    }
+
+
+    // this will be random number generators for the crystals button
+
+    $(".ruby").on("click", function() {
+        alert("It is working.");
+    });
+
+    $(".sapphire").on("click", function() {
+        alert("It is working.");
+    });
+
+    $(".teardrop").on("click", function() {
+        alert("It is working.");
+    });
+
+    $(".tourmaline").on("click", function() {
+        alert("It is working.");
+    });
+
+
 
 });
